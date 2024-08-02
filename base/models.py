8a1963508bd42d18909acd85738d1496e592897d
Braydon Giallombardo <braydon.giallombardo@gmail.com>
2024-08-01 23:57:18 -0400

    Add Media model
    Model for database containing information about each tv show, movie, etc

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Media(models.Model):
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    recommender = models.CharField(max_length=200)
+    tags = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    def __str__(self)-> str:
+        return self.title
--- base/views.py
+++ base/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Media
 
 def index(request) -> HttpResponse:
-    return render(request, 'index.html')
+    query = Media.objects.all()
+    context = {'medias' : query}
+    return render(request, 'base/index.html', context)
+
+def content(request, id) -> HttpResponse:
+    query = Media.objects.get(id=id) 
+    context = {'media': query}
+    return render(request, 'base/content.html', context)
