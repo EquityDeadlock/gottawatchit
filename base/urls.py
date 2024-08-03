541051ff89ba2e5bd46b4b15c024b32c97a2c1f1
Braydon Giallombardo <braydon.giallombardo@gmail.com>
2024-08-03 12:35:13 -0400

    Add create and update media functionality

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="home"),
     path("content/<str:id>/", views.content, name="content"),
-    path("register/", views.register, name="register")
+    path("register/", views.register, name="register"),
+    path("create-media/", views.createMedia, name="create-media"),
+    path("update-media/<str:id>/", views.updateMedia, name="update-media"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
 from .models import Media
+from .forms import MediaForm
 
 def index(request) -> HttpResponse:
     query = Media.objects.all()
@@ -14,3 +15,26 @@ def content(request, id) -> HttpResponse:
 
 def register(request) -> HttpResponse:
     return render(request, 'base/register.html')
+
+def createMedia(request: HttpRequest) -> HttpResponse:
+    form = MediaForm()
+    if request.method == 'POST':
+        form = MediaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/create_media.html', context)
+
+def updateMedia(request: HttpRequest, id) -> HttpResponse:
+    media = Media.objects.get(id=id)
+    form = MediaForm(instance=media)
+
+    if request.method == 'POST':
+        form = MediaForm(request.POST, instance=media)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/update_media.html', context)
+
