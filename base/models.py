11216336e8200749eae5dcdc91b0ce08c6789729
Braydon Giallombardo <braydon.giallombardo@gmail.com>
2024-08-06 23:31:22 -0400

    Change Media tags datatype to Tag foreign key

--- base/models.py
+++ base/models.py
@@ -1,21 +1,5 @@
 from django.db import models
 
-
-class Media(models.Model):
-    title = models.CharField(max_length=200)
-    genre = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    recommender = models.CharField(max_length=200)
-    tags = models.CharField(max_length=200)
-    created = models.DateTimeField(auto_now_add=True)
-    update = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ['-update', '-created']
-
-    def __str__(self)-> str:
-        return self.title
-
 class Tag(models.Model):
     name = models.CharField(max_length=30)
     count = models.PositiveIntegerField()
@@ -25,3 +9,19 @@ class Tag(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+class Media(models.Model):
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    recommender = models.CharField(max_length=200)
+    tags = models.ForeignKey(Tag, on_delete=models.SET_NULL, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-update', '-created']
+
+    def __str__(self)-> str:
+        return self.title
+
