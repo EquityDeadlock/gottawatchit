a5fa25fd708df100223c46d91b412ef7e6357d68
Braydon Giallombardo <braydon.giallombardo@gmail.com>
2024-08-07 21:16:26 -0400

    Add TagForm

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
-from .models import Media
+from .models import Media, Tag
 
 
 class MediaForm(ModelForm):
     class Meta:
         model = Media
+        fields = '__all__'
+
+class TagForm(ModelForm):
+    class Meta:
+        model = Tag 
         fields = '__all__'
